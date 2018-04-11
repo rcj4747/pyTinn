ac44b9e1fc2da18c8f38d8ff235efadaf0288c3d
Robert C Jennings <robert.jennings@canonical.com>
2018-04-11 16:37:59 -0500

    Add packaging and tox

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Tinn',
+    version='0.0.1',
+    author='nvails',
+    author_email='nvails@github.com',
+    data_files=[('tinn.py')],
+    url='https://github.com/nvalis/pyTinn',
+    license='LICENSE',
+    description='Pythonic port of the Tinn library (https://github.com/glouw/tinn).',
+)
